91d67050a5e0c65e04eeb909146510e063cc3bc1
Ross Sheppard <ross.sheppard@hpe.com>
2017-06-19 13:06:37 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(path.join(__dirname, '../public')));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
@@ -21,6 +33,12 @@ io.on('connection', (socket) => {
       text: 'Hey, this is really cool.',
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: 'someone@test.com',
+    //   text: 'Hey, this is really really cool.',
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', (socket) => {
